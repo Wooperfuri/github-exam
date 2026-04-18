f51f209c9caa7bd6adf182498dfa9735bb3d7e52
Wooperfuri <marcadrian.padilla@fcpc.edu.ph>
2026-04-18 11:25:50 +0800

    Added expense input system and category selection loop

---
+++ midterm_solution.py
@@ -0,0 +1,22 @@
+# Expense Tracker Program
+
+# Ask for student details
+name = input("Student name: ")
+budget = float(input("Weekly budget: "))
+
+# Expense categories
+categories = [
+    "Food & Drinks",
+    "Transportation",
+    "Mobile / Internet",
+    "School Supplies",
+    "Entertainment"
+]
+
+examples = [
+    "Lunch, snacks, coffee",
+    "Bus, jeepney, ride-share",
+    "Load, data plan, WiFi",
+    "Notebook, pen, paper",
+    "Games, movies, hangout"
+]
